eb14892d2354b7af60743f7607dbb1761f57b3e4
levanliu <839119912@qq.com>
2024-05-30 19:36:55 +0800

    add .

--- install.sh
+++ install.sh
@@ -4,19 +4,6 @@ if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root"
   exit 1
 fi
-# Check dependencies
-if type curl &>/dev/null; then
-  echo "" &>/dev/null
-else
-  echo "You need to install 'curl' to use the chatgpt script."
-  exit
-fi
-if type jq &>/dev/null; then
-  echo "" &>/dev/null
-else
-  echo "You need to install 'jq' to use the chatgpt script."
-  exit
-fi
 
 # Installing imgcat if using iTerm
 if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
